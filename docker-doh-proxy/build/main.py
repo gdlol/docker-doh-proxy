827f8f6ff6d78ada70dfaa481aa717185095e82f
GordenOu <gordon.xwj@outlook.com>
2019-08-19 22:42:54 +0800

    Remove copy command.

--- docker-doh-proxy/build/main.py
+++ docker-doh-proxy/build/main.py
@@ -2,7 +2,5 @@ import subprocess
 
 subprocess.run(["pip", "install", "doh-proxy"], check=True)
 requirements = subprocess.check_output(["pip", "freeze"])
-with open("/root/source/requirements.txt", "wb") as file:
+with open("/root/requirements.txt", "wb") as file:
     file.write(requirements)
-subprocess.run(["cp", "--recursive", "/root/source/.", "/root/output"],
-               check=True)

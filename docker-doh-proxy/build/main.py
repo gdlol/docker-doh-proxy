1af07805764453f636cf1c0b0b48ced1ba1524d3
GordenOu <gordon.xwj@outlook.com>
2019-08-25 15:06:30 +0800

    Modify build source folder.

--- docker-doh-proxy/build/main.py
+++ docker-doh-proxy/build/main.py
@@ -2,5 +2,5 @@ import subprocess
 
 subprocess.run(["pip", "install", "doh-proxy"], check=True)
 requirements = subprocess.check_output(["pip", "freeze"])
-with open("/root/requirements.txt", "wb") as file:
+with open("/root/source/requirements.txt", "wb") as file:
     file.write(requirements)
